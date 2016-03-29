2385539e2b6d958f2bc84d59309afa882d454c91
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-03-29 18:32:07 -0500

    Add devices to TeleOp code

--- ftc-robotcontroller/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleopSkunk.java
+++ ftc-robotcontroller/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleopSkunk.java
@@ -1,17 +1,22 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.lasarobotics.library.controller.ButtonState;
 import com.lasarobotics.library.controller.Controller;
 import com.lasarobotics.library.drive.Tank;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Skunk (TM) Teleop
  */
 public class TeleopSkunk extends OpMode {
 
-    DcMotor leftFront, rightFront, leftBack, rightBack;
-    Controller one;
+    DcMotor leftFront, rightFront, leftBack, rightBack,liftA,liftB,winch,intake;
+    Servo leftHook,rightHook;
+    Controller one,two;
+    boolean hooked;
+    boolean intakeBool;
 
     @Override
     public void init() {
@@ -19,17 +24,66 @@ public class TeleopSkunk extends OpMode {
         rightFront = hardwareMap.dcMotor.get("rf");
         leftBack = hardwareMap.dcMotor.get("lb");
         rightBack = hardwareMap.dcMotor.get("rb");
+        winch = hardwareMap.dcMotor.get("winch");
+        liftA = hardwareMap.dcMotor.get("liftA");
+        liftB = hardwareMap.dcMotor.get("liftB");
+        intake = hardwareMap.dcMotor.get("intake");
 
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        leftHook = hardwareMap.servo.get("leftHook");
+        rightHook = hardwareMap.servo.get("rightHook");
+
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
 
         one = new Controller(gamepad1);
+        two = new Controller(gamepad2);
+
+        leftHook.setPosition(0);
+        rightHook.setPosition(1);
     }
 
     @Override
     public void loop() {
         one.update(gamepad1);
+        two.update(gamepad2);
+
         telemetry.addData("Controller One Connected", one.isConnected());
+        telemetry.addData("Controller Two Connected", two.isConnected());
+
+        //Hooks
+        if (one.x == ButtonState.PRESSED){
+            hooked = !hooked;
+        }
+        if (hooked){
+            leftHook.setPosition(1);
+            rightHook.setPosition(0);
+        }
+        else {
+            leftHook.setPosition(0);
+            rightHook.setPosition(1);
+        }
+
+        //Winch
+        winch.setPower(two.right_stick_y);
+
+        //Lift
+        liftA.setPower(two.left_stick_y);
+        liftB.setPower(-two.left_stick_y);
+
+        //Intake
+        if (two.a == ButtonState.PRESSED){
+            intakeBool = !intakeBool;
+        }
+        if (two.left_bumper == ButtonState.HELD || two.left_bumper == ButtonState.PRESSED){
+            intake.setPower(-1);
+        }
+        else if(intakeBool) {
+            intake.setPower(1);
+        }
+        else {
+            intake.setPower(0);
+        }
+
 
         Tank.motor4(leftFront, rightFront, leftBack, rightBack, one.left_stick_y, one.right_stick_y);
     }
