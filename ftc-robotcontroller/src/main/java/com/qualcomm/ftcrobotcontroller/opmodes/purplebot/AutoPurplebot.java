ecdb355879b45860bb4bdbaf3972ad60f4164304
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-04-01 09:40:56 -0500

    Write simple NavX rotation nav lib

--- ftc-robotcontroller/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/purplebot/AutoPurplebot.java
+++ ftc-robotcontroller/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/purplebot/AutoPurplebot.java
@@ -5,7 +5,6 @@ import org.lasarobotics.vision.detection.objects.Rectangle;
 import org.lasarobotics.vision.ftc.resq.Beacon;
 import org.lasarobotics.vision.opmode.LinearVisionOpMode;
 import org.lasarobotics.vision.util.ScreenOrientation;
-import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Size;
 
@@ -58,18 +57,30 @@ public class AutoPurplebot extends LinearVisionOpMode {
         //beacon.setAnalysisBounds(new Rectangle(0, 0, height, width));
     }
 
+
+    private void initRobot() {
+
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         //Initialize Vision
-        initVision();
+        //initVision();
+
+        //Initialize robot control
+        initRobot();
 
         //Wait for the match to begin
         waitForStart();
 
+        //Robot control
+
+
+
         //Main loop
         //Camera frames and OpenCV analysis will be delivered to this method as quickly as possible
         //This loop will exit once the opmode is closed
-        while (opModeIsActive()) {
+        /**while (opModeIsActive()) {
             //Log a few things
             telemetry.addData("Beacon Color", beacon.getAnalysis().getColorString());
             telemetry.addData("Beacon Location (Center)", beacon.getAnalysis().getLocationString());
@@ -98,6 +109,6 @@ public class AutoPurplebot extends LinearVisionOpMode {
 
             //Wait for a hardware cycle to allow other processes to run
             waitOneFullHardwareCycle();
-        }
+         }**/
     }
 }
